10f46be5c0b1bdc3e7be9de01b2cd2371535c00b
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2024-05-07 14:33:50 +0300

    create: repositories for the bids, jobs, contractor, employee records

---
+++ myKazi/src/main/java/com/example/myKazi/BidsRepository.java
@@ -0,0 +1,7 @@
+package com.example.myKazi;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BidsRepository extends CrudRepository<Bids, Integer> {
+
+}
---
+++ myKazi/src/main/java/com/example/myKazi/JobsRepository.java
@@ -0,0 +1,8 @@
+package com.example.myKazi;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface JobsRepository extends CrudRepository<Jobs, Integer> {
+
+    
+}
